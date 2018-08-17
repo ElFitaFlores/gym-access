4b7a7b0dac6d102d62c55daa155521551ef88bf1
Rafael Flores <fita500@gmail.com>
2018-08-16 23:05:15 -0600

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+html, body {
+    background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+    height: 100vh;
+            }
+
+            .flex-center {
+    align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+    position: relative;
+}
+
+            .top-right {
+    position: absolute;
+    right: 10px;
+                top: 18px;
+            }
+
+            .content {
+    text-align: center;
+            }
+
+            .title {
+    font-size: 84px;
+            }
+
+            .links > a {
+    color: #636b6f;
+    padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+    margin-bottom: 30px;
+            }
+        </style>
+        <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.css"/>
+        <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/gijgo@1.9.10/js/gijgo.min.js" type="text/javascript"></script>
+        <link href="https://cdn.jsdelivr.net/npm/gijgo@1.9.10/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
+
+            <div class="container">
+                @yield('content')
+            </div>
+        </div>
+    </body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.app')
+
+@section('content')
+<h1 class="">
+    Accesos al gimnasio
+</h1>
+<div class="row">
+    <div class="col">
+        <label for="min">Fecha inicio</label>
+        <input type="text" id="min">
+    </div>
+    <div class="col">
+        <label for="max">Fecha fin</label>
+        <input type="text" id="max">
+    </div>
+</div>
+<hr>
+<div class="row">
+    <div class="table-responsive">
+        <table class="table table-bordered table-striped" id="usersTable">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Fecha</th>
+                    <th>Hora</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    </div>
+</div>
+<script>
+    var table;
+    $(function(){
+        var users = [
+          {
+            "id": 1,
+            "fecha_hora_ingreso": "2018-08-13T21:04:35.31",
+            "usuario": "usuario_test"
+          },
+          {
+            "id": 4,
+            "fecha_hora_ingreso": "2018-08-13T21:13:40.357",
+            "usuario": "test2"
+          },
+          {
+            "id": 5,
+            "fecha_hora_ingreso": "2018-08-14T03:16:01.127",
+            "usuario": "test3"
+          }
+        ];
+
+        /*var user = [];
+        $.ajax({
+          url: "http://serviciosrestumg.azurewebsites.net/api/control_usuarios",
+        }).done(function(data){
+          user = data;
+        });*/
+
+      $('#min').datepicker({
+        value: moment().startOf('month').format('YYYY-MM-DD'),
+        uiLibrary: 'bootstrap4',
+        format: 'yyyy-mm-dd',
+        change: function(e){
+          table.draw();
+        },
+      });
+      $('#max').datepicker({
+        value: moment().endOf('month').format('YYYY-MM-DD'),
+        uiLibrary: 'bootstrap4',
+        format: 'yyyy-mm-dd',
+        change: function(e){
+          table.draw();
+        },
+      });
+
+      $.fn.dataTable.ext.search.push(
+        function (settings, data, dataIndex) {
+          var min = moment($('#min').datepicker().value());
+          var max = moment($('#max').datepicker().value());
+          var startDate = moment(data[2]);
+          if (min == null && max == null) { return true; }
+          if (min == null && startDate <= max) { return true;}
+          if(max == null && startDate >= min) {return true;}
+          if (startDate <= max && startDate >= min) { return true; }
+          return false;
+        }
+      );
+
+        updateTable(users);
+    });
+
+    function updateTable(object){
+      var parsedObject = $.map(object,function(i){
+        var fecha = moment(i.fecha_hora_ingreso);
+        return [{
+          "id": i.id,
+          "usuario": i.usuario,
+          "fecha": fecha.format('YYYY-MM-DD'),
+          "hora": fecha.format('hh:mm a'),
+        }]
+      });
+
+      table = $("#usersTable").DataTable({
+        data: parsedObject,
+        columns: [
+          {data: 'id'},
+          {data: 'usuario'},
+          {data: 'fecha'},
+          {data: 'hora'},
+        ],
+      });
+    }
+</script>
+@endsection
